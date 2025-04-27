3220f2e3a207fee6fa18c9492de10f8ece90a104
dubeyshubham108 <dubeyshubham154@gmail.com>
2025-04-27 06:53:10 +0530

    added the contact, testimonial components

--- educity/src/App.jsx
+++ educity/src/App.jsx
@@ -5,6 +5,9 @@ import Programs from './components/Programs/Programs.jsx'
 import Title from './components/Title/Title.jsx'
 import './App.css'
 import About from './components/About/About.jsx'
+import Campus from './components/Campus/Campus.jsx'
+import Testimonial from './components/Testimonial/Testimonial.jsx'
+import Contact from './components/Contact/Contact.jsx'
 
 function App() {
 
@@ -16,6 +19,12 @@ function App() {
         <Title subTitle='OUR PROGRAM' title='What We Offer' / >
         <Programs/>
         <About/>
+        <Title subTitle='Gallery' title='Campus Photos' / >
+        <Campus/>
+        <Title subTitle='TESTIMONIAL' title='What Student Says' / >
+        <Testimonial/>
+        <Title subTitle='Contact Us' title='Get in Touch' / >
+        <Contact/>
       </div>
     </div>
   )
